6d8335bff7c11bed49efaa3707ddc6342689474e
Morten Gross <mg@vuffeli.dk>
2021-02-18 13:47:49 +0100

    Initial functions

--- script.js
+++ script.js
@@ -5,3 +5,75 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
     console.log("init");
 }
+
+function getColor() {
+    console.log("getColor");
+}
+
+function hexToRGB() {
+    console.log("hexToRGB");
+}
+
+function rgbToHSL() {
+    console.log("rgbToHSL");
+}
+
+function calculateHarmony() {
+    console.log("calculateHarmony");
+}
+
+function calcAnalogous() {
+    console.log("calcAnalogous");
+}
+
+function calcMonochromatic() {
+    console.log("calcMonochromatic");
+}
+
+function calcTriad() {
+    console.log("calcTriad");
+}
+
+function calcComplementary() {
+    console.log("calcComplementary");
+}
+
+function calcCompound() {
+    console.log("calcCompound");
+}
+
+function calcShades() {
+    console.log("calcShades");
+}
+
+function displayColorInfo() {
+    console.log("displayColorInfo");
+}
+
+function hslToRGB() {
+    console.log("hslToRGB");
+}
+
+function rgbToHex() {
+    console.log("rgbToHex");
+}
+
+function rgbToCSS() {
+    console.log("rgbToCSS");
+}
+
+function showHex() {
+    console.log("showHex");
+}
+
+function showRGB() {
+    console.log("showRGB");
+}
+
+function showHSL() {
+    console.log("showHSL");
+}
+
+function changeBoxColor() {
+    console.log("changeBoxColor");
+}
